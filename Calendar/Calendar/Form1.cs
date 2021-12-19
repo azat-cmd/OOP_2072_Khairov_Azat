2d68ca1253611738647dce94d4c5bba58bf8e3ee
Azat Khairov <azath_2002@mail.ru>
2021-12-19 20:44:11 +0300

    add calendar

---
+++ Calendar/Calendar/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calendar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //enum Month
+        //{
+        //    Jan = 1,
+        //    Feb,
+        //    Mar,
+        //    Apr,
+        //    May,
+        //    Jun,
+        //    Jul,
+        //    Aug,
+        //    Sep,
+        //    Oct,
+        //    Nov,
+        //    Dec
+        //}
+        MyDate calend = new MyDate();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SetDateButton_Click(object sender, EventArgs e)
+        {
+            calend.SetDate((int)(x.Value), (int)(y.Value),(int)(z.Value));
+            x.Value = calend.x;
+            y.Value = calend.y;
+            z.Value = calend.z;
+            NextDateButton.Enabled = true;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void NextDateButton_Click(object sender, EventArgs e)
+        {
+            calend.NextDate();
+            x.Value = calend.x;
+            y.Value = calend.y;
+            z.Value = calend.z;
+        }
+    }
+}
